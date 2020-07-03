38481c5961bcd15ff78b4af084f6f8a477949ce4
Sean Hammon <git@sean-hammon.com>
2020-07-03 16:09:28 -0600

    Whitespace.

--- src/app/store/session.store.ts
+++ src/app/store/session.store.ts
@@ -4,23 +4,23 @@ import { BehaviorSubject } from 'rxjs';
 import { Galleries } from '../galleries/galleries';
 import { Gallery } from '../galleries/gallery.interface';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class SessionStore {
 
-    loading$: BehaviorSubject<boolean>;
-    gallery$: BehaviorSubject<Gallery>;
+  loading$: BehaviorSubject<boolean>;
+  gallery$: BehaviorSubject<Gallery>;
 
-    constructor(
-        private galleryProvider: GalleryProvider
-    ) {
-        this.loading$ = new BehaviorSubject(true);
-        this.gallery$ = new BehaviorSubject(null);
-    }
+  constructor(
+    private galleryProvider: GalleryProvider
+  ) {
+    this.loading$ = new BehaviorSubject(true);
+    this.gallery$ = new BehaviorSubject(null);
+  }
 
-    selectGallery(id: string) {
-        const g = {...Galleries[id]};
-        g.children = this.galleryProvider.childGalleries(g);
-        this.gallery$.next(g);
-    }
+  selectGallery(id: string) {
+    const g = { ...Galleries[id] };
+    g.children = this.galleryProvider.childGalleries(g);
+    this.gallery$.next(g);
+  }
 
 }
--- src/app/widgets/spinner/spinner.component.ts
+++ src/app/widgets/spinner/spinner.component.ts
@@ -5,57 +5,57 @@ import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 @Component({
-    selector: 'app-spinner',
-    templateUrl: './spinner.component.html',
-    styleUrls: ['./spinner.component.sass'],
-    animations: [
-        trigger('spinnerState', [
-            state('hidden', style({
-                opacity: 0
-            })),
-            state('visible', style({
-                opacity: 1
-            })),
-            transition('hidden => visible', animate('10ms')),
-            transition('visible => hidden', animate('1500ms ease-out'))
-        ])
-    ]
+  selector: 'app-spinner',
+  templateUrl: './spinner.component.html',
+  styleUrls: ['./spinner.component.sass'],
+  animations: [
+    trigger('spinnerState', [
+      state('hidden', style({
+        opacity: 0
+      })),
+      state('visible', style({
+        opacity: 1
+      })),
+      transition('hidden => visible', animate('10ms')),
+      transition('visible => hidden', animate('1500ms ease-out'))
+    ])
+  ]
 
 })
 export class SpinnerComponent implements OnInit, OnDestroy {
 
-    loading: string;
-    display: boolean;
-    unsub$: Subject<boolean>;
+  loading: string;
+  display: boolean;
+  unsub$: Subject<boolean>;
 
-    constructor(
-        private session: SessionStore
-    ){
+  constructor(
+    private session: SessionStore
+  ) {
+    this.display = true;
+  }
+
+  ngOnInit() {
+
+    this.session.loading$
+      .pipe(
+        takeUntil(this.unsub$)
+      )
+      .subscribe(loading => {
+        this.loading = loading ? 'visible' : 'hidden';
         this.display = true;
-    }
+      });
 
-    ngOnInit() {
-
-      this.session.loading$
-        .pipe(
-          takeUntil(this.unsub$)
-        )
-        .subscribe(loading => {
-          this.loading = loading ? 'visible' : 'hidden';
-          this.display = true;
-        });
-
-    }
+  }
 
 
-    ngOnDestroy() {
-      this.unsub$.next(true);
-      this.unsub$.complete();
-    }
+  ngOnDestroy() {
+    this.unsub$.next(true);
+    this.unsub$.complete();
+  }
 
 
-    animationDone($event) {
-        this.display = false;
-    }
+  animationDone($event) {
+    this.display = false;
+  }
 
 }
