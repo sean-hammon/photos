2e12f7a64a248d26d1c24152ead202e9a6a2a911
Sean Hammon <git@sean-hammon.com>
2020-07-03 16:11:20 -0600

    gallery.provider: get a random photo.

--- src/app/galleries/gallery.provider.ts
+++ src/app/galleries/gallery.provider.ts
@@ -1,9 +1,12 @@
 import { Injectable } from "@angular/core";
 import { Gallery } from './gallery.interface';
 import { Galleries } from './galleries';
+import { Photo } from '../photos/photo/photo.interface';
+import { Photos } from '../photos/photos';
 
 @Injectable({providedIn: 'root'})
 export class GalleryProvider {
+
 
     childGalleries(parent: Gallery) {
         return parent.children.map(childId => {
@@ -13,5 +16,11 @@ export class GalleryProvider {
 
     photos(gallery: Gallery) {
         return [];
+    }
+
+    randomPhoto(gallery: Gallery): Photo {
+        const rnd = Math.floor(Math.random() * gallery.photos.length);
+        const hash = gallery.photos[rnd];
+        return Photos[hash];
     }
 }
