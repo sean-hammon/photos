c30200d24e8d81735db3665d599e2ed7a3c8dae7
Sean Hammon <git@sean-hammon.com>
2020-07-03 16:10:58 -0600

    photo.component: start loading photos.

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { environment } from './../../../environments/environment';
+import { style } from '@angular/animations';
+import { GalleryProvider } from './../../galleries/gallery.provider';
+import { SessionStore } from './../../store/session.store';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Galleries } from 'src/app/galleries/galleries';
 
 @Component({
   selector: 'app-photo',
   templateUrl: './photo.component.html',
   styleUrls: ['./photo.component.sass']
 })
-export class PhotoComponent implements OnInit {
+export class PhotoComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('one') one: ElementRef;
+  @ViewChild('two') two: ElementRef;
+
+  constructor(
+    private galleries: GalleryProvider,
+    private route: ActivatedRoute,
+    private session: SessionStore
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.route.params
+      .subscribe(p => {
+        if (p.hash) {
+
+        } else {
+          this.session.selectGallery(environment.homeGallery);
+          const gallery = Galleries[environment.homeGallery];
+          const photo = this.galleries.randomPhoto(gallery);
+          this.one.nativeElement.style.backgroundImage = `url(${photo.photo})`;
+        }
+      });
   }
 
 }
---
+++ src/app/photos/photo/photo.interface.ts
@@ -0,0 +1,4 @@
+export interface Photo {
+    thumb: string;
+    photo: string;
+}
