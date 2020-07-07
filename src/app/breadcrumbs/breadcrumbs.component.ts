d115689bc6d50277dcacae5e3dc10ca1e49ae2f3
Sean Hammon <git@sean-hammon.com>
2020-07-07 15:04:38 -0600

    breadcrumbs: set up to build dynamically.

--- src/app/breadcrumbs/breadcrumbs.component.ts
+++ src/app/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,4 @@
-import { takeUntil } from 'rxjs/operators';
+import { takeUntil, filter } from 'rxjs/operators';
 import { BehaviorSubject, Subject } from 'rxjs';
 import { SessionStore } from './../store/session.store';
 import { Component, OnInit } from '@angular/core';
@@ -11,22 +11,33 @@ import { Gallery } from '../galleries/gallery.interface';
 })
 export class BreadcrumbsComponent implements OnInit {
 
+  public ancestors: Gallery[];
+  public leaves: Gallery[];
+
   private unsub$: Subject<null>;
 
   constructor(
     private session: SessionStore
-  ) { }
+  ) {
+    this.ancestors = [];
+  }
 
   ngOnInit(): void {
     this.unsub$ = new Subject();
 
     this.session.gallery$
-      .pipe(takeUntil(this.unsub$))
+      .pipe(
+        takeUntil(this.unsub$),
+        filter(gallery => !!gallery)
+      )
       .subscribe(g => this.updateCrumbs(g));
   }
 
   updateCrumbs(gallery: Gallery) {
-
+    if (!this.ancestors.length) {
+      this.ancestors.push(gallery);
+    }
+    this.leaves = gallery.children as Gallery[];
   }
 
 }
