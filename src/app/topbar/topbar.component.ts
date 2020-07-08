2ad162e42d6c5c3c5601ca7e738255448cb743e6
Sean Hammon <git@sean-hammon.com>
2020-07-08 09:15:12 -0600

    topbar: update title for selected gallery.

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -1,6 +1,7 @@
 import { SessionStore } from '@app/store/session.store';
 import { Component, OnInit } from '@angular/core';
 import { filter } from 'rxjs/operators';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-topbar',
@@ -13,15 +14,25 @@ export class TopbarComponent implements OnInit {
   pageDescription: string;
 
   constructor(
+    private route: ActivatedRoute,
+    private router: Router,
     private session: SessionStore
   ) { }
 
   ngOnInit(): void {
+
     this.session.photo$
     .pipe(filter(photo => !!photo))
     .subscribe(p => {
       this.pageTitle = p.title;
       this.pageDescription = p.short_desc;
+    });
+
+    this.session.gallery$
+    .pipe(filter(gallery => !!gallery))
+    .subscribe(g => {
+      this.pageTitle = g.title;
+      this.pageDescription = g.description;
     });
   }
 
