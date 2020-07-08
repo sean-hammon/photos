bf7b1f59df1b674a528aa022ac38043a92bdd4f6
Sean Hammon <git@sean-hammon.com>
2020-07-08 09:16:57 -0600

    gallery.component: set current gallery based on route.

--- src/app/galleries/gallery/gallery.component.ts
+++ src/app/galleries/gallery/gallery.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { SessionStore } from '@app/store/session.store';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css']
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private unsub$: Subject<null>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private session: SessionStore
+  ) { }
 
   ngOnInit(): void {
+
+    this.unsub$ = new Subject();
+    this.router.events
+      .subscribe(event => {
+        // All subsequent routes to a gallery.
+        if (event instanceof NavigationEnd) {
+          this.setGallery();
+        }
+      });
+
+    // Initial load of component
+    this.setGallery();
+
+  }
+
+  ngOnDestroy() {
+    this.unsub$.next();
+    this.unsub$.complete();
+  }
+
+  setGallery() {
+
+    this.route.params
+    .pipe(takeUntil(this.unsub$))
+    .subscribe(p => {
+      this.session.selectGallery(p.ghash);
+    });
+
   }
 
 }
