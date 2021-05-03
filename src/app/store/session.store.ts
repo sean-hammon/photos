dc2b61ea8f5efd5a172ca1a224d852d15848885e
Sean Hammon <git@sean-hammon.com>
2021-05-02 18:46:31 -0600

    session.store: start using the session store to get api URL.

--- src/app/store/session.store.ts
+++ src/app/store/session.store.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { GalleryProvider, Gallery } from '@app/galleries';
 import { Photo } from '@app/photos';
+import { environment } from '@env';
 
 @Injectable({ providedIn: 'root' })
 export class SessionStore {
@@ -9,6 +10,7 @@ export class SessionStore {
   loading$: BehaviorSubject<boolean>;
   gallery$: BehaviorSubject<Gallery>;
   photo$: BehaviorSubject<Photo>;
+  api$: BehaviorSubject<string>;
 
   constructor(
     private galleries: GalleryProvider
@@ -16,6 +18,7 @@ export class SessionStore {
     this.loading$ = new BehaviorSubject(true);
     this.gallery$ = new BehaviorSubject(null);
     this.photo$ = new BehaviorSubject(null);
+    this.api$ = new BehaviorSubject<string>(environment.api);
   }
 
   setPhoto(photo: Photo) {
